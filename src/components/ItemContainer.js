334a445fd56c669e6f0d453078a4ae66ed71c1a0
David Stinnette <dastinnette@gmail.com>
2023-10-17 08:43:48 -0400

    Added newItem feature and form

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -3,7 +3,9 @@ import ItemCard from './ItemCard'
 function ItemContainer({ searchValue,snapshots }) {
 
     const foundSnapShots = snapshots.filter((snapshot)=> 
-    snapshot.explanation.toLowerCase().includes(searchValue.toLowerCase()) || snapshot.title.toLowerCase().includes(searchValue.toLowerCase()) || snapshot.date.toLowerCase().includes(searchValue.toLowerCase()))
+        snapshot.explanation.toLowerCase().includes(searchValue.toLowerCase()) || 
+        snapshot.title.toLowerCase().includes(searchValue.toLowerCase()) || 
+        snapshot.date.toLowerCase().includes(searchValue.toLowerCase()))
 
     const renderSnapShots = foundSnapShots.map((snapshot) => (
             <ItemCard
