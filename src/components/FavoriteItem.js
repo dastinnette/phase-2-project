5544b56a150dd5026d44ad00be4e9334cf57e9c0
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-10-19 08:08:54 -0500

    added trac

--- src/components/FavoriteItem.js
+++ src/components/FavoriteItem.js
@@ -6,11 +6,13 @@ function FavoriteItem({ favorite, draggable, onMouseDown, onMouseUp, onMouseMove
     function handleFavoriteClick(){
         const isConfirmed = window.confirm(`are you sure you want to remove this item from the list?`)
     if (isConfirmed) {
+        //^ is boolean that is on homepage that renders then another boolean to show window with button to delete
         fetch(`http://localhost:6001/favorites/${id}`, {method: 'DELETE'})
         .then(r =>{
             if(r.ok)
             console.log(`removed`)
             setFavoritesBar((favoriteBar) => favoriteBar.filter((favorite) => favorite.id !==id))
+            // -> bolean = state in homepage passed thru context = ^
         } 
         )
     }
