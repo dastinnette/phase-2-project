cc1b2b8caba8e1e469dc6f451a3bc8009714087e
David Stinnette <dastinnette@gmail.com>
2023-10-19 11:18:30 -0400

    Improved detail page and new form background blue

--- src/components/DetailedPage.js
+++ src/components/DetailedPage.js
@@ -8,14 +8,29 @@ function DetailedPage(){
 
     return(
         <div>
-            <img className="detail-image"
-            src={snapshot?.hdurl}
-            alt={snapshot?.title}
-        />
-        <h1 className="detail-title">{snapshot?.title}</h1>
-        <p className="detail-date">{snapshot?.date}</p>
-        <p className="detail-explanation">{snapshot?.explanation}</p>
-    </div>
+            <div>
+                <img className="detail-image"
+                src={snapshot?.hdurl}
+                alt={snapshot?.title}
+            />
+                <h1 className="detail-title">{snapshot?.title}</h1>
+                <p className="detail-date">{snapshot?.date}</p>
+                <p className="detail-explanation">{snapshot?.explanation}</p>
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+        </div>
     )
 }
 export default DetailedPage;
